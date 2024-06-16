229b96997c97bdcba6226c8da4b8c6151c34f460
0xMayo <wakjiraalemayehu@gmail.com>
2024-06-16 17:16:05 +0200

    setup a userSchema

---
+++ Lirium-backend/models/User.mjs
@@ -0,0 +1,72 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please enter a valid email',
+    ],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'manager'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordTokenExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  course: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Course',
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+userSchema.methods.validatePassword = async function (passwordToCheck) {
+  return await bcrypt.compare(passwordToCheck, this.password);
+};
+
+userSchema.methods.generateToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_TTL,
+  });
+};
+
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.resetPasswordTokenExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+export default mongoose.model('User', userSchema);
