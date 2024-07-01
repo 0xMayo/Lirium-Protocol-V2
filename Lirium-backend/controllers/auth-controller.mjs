23fa462b4ee603d5232f8ef9d5582f85bb98e7b0
0xMayo <wakjiraalemayehu@gmail.com>
2024-07-01 15:42:28 +0200

    Added an auth controller

--- Lirium-backend/controllers/auth-controller.mjs
+++ Lirium-backend/controllers/auth-controller.mjs
@@ -1,3 +1,149 @@
 import User from '../models/User.mjs';
-import ErrorResponse
- from '../models/ErrorResponse.mjs';
+import ErrorResponse from '../models/ErrorResponse.mjs';
+import { asyncHandler } from '../middleware/asyncHandler.mjs';
+
+// @desc    Register user
+// @route   POST /api/v1/auth/register
+// @access  Public
+
+export const register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({ name, email, password, role });
+
+  createAndSendToken(user, 201, res);
+});
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+
+export const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const isMatch = await user.validatePassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  createAndSendToken(user, 200, res);
+});
+
+// @desc    Get current logged in user
+// @route   GET /api/v1/auth/me
+// @access  Private
+
+export const getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    statusCode: 200,
+    data: user,
+  });
+});
+
+// @desc    Update user details
+// @route   PUT /api/v1/auth/updatedetails
+// @access  Private
+
+export const updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    statusCode: 200,
+    data: user,
+  });
+});
+
+// @desc    Update password
+// @route   PUT /api/v1/auth/updatepassword
+// @access  Private
+
+export const updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  if (!(await user.validatePassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Felaktigt lösenord', 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  createAndSendToken(user, 200, res);
+});
+
+// @desc   Forgot password
+// @route  POST /api/v1/auth/forgotpassword
+// @access Public
+
+export const forgotPassword = asyncHandler(async (req, res, next) => {
+  const email = req.body.email;
+
+  if (!email) {
+    return next(new ErrorResponse('Please provide an email address', 400));
+  }
+
+  let user = await User.findOne({ email });
+
+  if (!user) return next(new ErrorResponse('There is no user with that email', 400));
+
+  const resetToken = user.createResetPasswordToken();
+  await user.save({ validateBeforeSave: false });
+
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/resetpassword/${resetToken}`;
+
+  res.status(200).json({
+    success: true,
+    statusCode: 201,
+    data: { token: resetToken, url: resetUrl },
+  });
+});
+
+// @desc   Reset password
+// @route  PUT /api/v1/auth/resetpassword/:token
+// @access Public
+
+export const resetPassword = asyncHandler(async (req, res, next) => {
+  const password = req.body.password;
+  const token = req.params.token;
+
+  if (!password) return next(new ErrorResponse('Please provide a password', 400));
+
+  let user = await User.findOne({ resetPasswordToken: token });
+
+  user.password = password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordTokenExpire = undefined;
+
+  await user.save();
+
+  createAndSendToken(user, 200, res);
+});
+
+const createAndSendToken = (user, statusCode, res) => {
+    const token = user.generateToken();
+  
+    res.status(statusCode).json({ success: true, statusCode, token });
+  };
--- Lirium-backend/routes/transaction-routes.mjs
+++ Lirium-backend/routes/transaction-routes.mjs
@@ -6,6 +6,5 @@ const router = express.Router();
 router.route("/").post(addTransaction);
 router.route("/transactions").get(getTransactionPool);
 router.route("/mine").get(mineTransactions);
-// router.route("/broadcast").post(broadcastTransaction);
 
 export default router;
